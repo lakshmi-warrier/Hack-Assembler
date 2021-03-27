e9c316ff87ed15a48af5a4f560b36134a9e52dd7
Lakshmi Warrier <lakshmi0105warrier@gmail.com>
2021-03-27 19:07:17 +0530

    Created Assembler

---
+++ Assembler.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Assembler {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter the input asm file path");
+        String source_file = sc.nextLine().trim();
+
+        while(!source_file.substring(source_file.length()-4, source_file.length()).equals(".asm"))
+        {
+            System.out.println("Input is not an asm file");
+            source_file = sc.nextLine();
+        }
+
+        System.out.println("\nEnter the output hack file path");
+        String target_file = sc.nextLine().trim();
+        
+        while(!target_file.substring(target_file.length()-5, target_file.length()).equals(".hack"))
+        {
+            System.out.println("Output file is not a hack file");
+            source_file = sc.nextLine();
+        }
+
+        Parser p1 = new Parser(source_file);
+        //System.out.println(p1.translate());
+
+        try {
+            FileWriter fw = new FileWriter(target_file);
+            fw.write(p1.translate());
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
