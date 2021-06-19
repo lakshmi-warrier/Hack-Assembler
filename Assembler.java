903ac7164749c6578cd99e21145610f0cca08c3b
lakshmi-warrier <lakshmi0105warrier@gmail.com>
2021-06-19 10:27:03 +0530

    commit

--- Assembler.java
+++ Assembler.java
@@ -8,17 +8,15 @@ public class Assembler {
         System.out.println("\nEnter the input asm file path");
         String source_file = sc.nextLine().trim();
 
-        while(!source_file.substring(source_file.length()-4, source_file.length()).equals(".asm"))
-        {
+        while (!source_file.substring(source_file.length() - 4, source_file.length()).equals(".asm")) {
             System.out.println("Input is not an asm file");
             source_file = sc.nextLine();
         }
 
         System.out.println("\nEnter the output hack file path");
         String target_file = sc.nextLine().trim();
-        
-        while(!target_file.substring(target_file.length()-5, target_file.length()).equals(".hack"))
-        {
+
+        while (!target_file.substring(target_file.length() - 5, target_file.length()).equals(".hack")) {
             System.out.println("Output file is not a hack file");
             source_file = sc.nextLine();
         }
@@ -32,6 +30,6 @@ public class Assembler {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        sc.close();
     }
 }
